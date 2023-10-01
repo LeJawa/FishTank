bd2ae204b66834eab45eabd9765451afa5d2f1c5
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2023-10-01 15:57:33 +0200

    Draw 2x2 squares instead of circles

--- scripts/Point.cs
+++ scripts/Point.cs
@@ -4,6 +4,7 @@ using System;
 public partial class Point : Node2D
 {
 	public override void _Draw(){
-		DrawCircle(new Vector2(0, 0), 3, new Color(1, 1, 1, 1));
+		// DrawCircle(new Vector2(0, 0), 3, new Color(1, 1, 1, 1));
+		DrawRect(new Rect2(Vector2.Zero, Vector2.One * 2), new Color(1, 1, 1, 1));
 	}
 }
