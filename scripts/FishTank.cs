84efc13a6c4a5ed1fa18773fd5f7ef66813714d2
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2023-10-01 19:35:30 +0200

    Start with 150 fishes

--- scripts/FishTank.cs
+++ scripts/FishTank.cs
@@ -21,6 +21,8 @@ public partial class FishTank : Node2D
 	public int TopMargin => 0;
 	public int BottomMargin => tankSize.Y;
 
+	private Random random = new Random();
+
     public override void _Ready()
     {
 		var size = DisplayServer.WindowGetSize();
@@ -32,26 +34,39 @@ public partial class FishTank : Node2D
 		Fishes = GetTree().GetNodesInGroup("Fish").Cast<Fish>().ToList();
 
 		Instance = this;
+
+		// Spawn 150 fishes
+		for (int i = 0; i < 150; i++)
+		{
+			AddFishAtRandom();
+		}
     }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
 		// check if the space bar is pressed
 		if (Input.IsActionPressed("click") && timer.IsStopped())
         {
-            AddFish();
+            AddFishAtMouse();
         }
     }
 
-    private void AddFish()
+	private void AddFishAtMouse(){
+		AddFish(GetGlobalMousePosition());
+	}
+
+    private void AddFishAtRandom()
     {
-		// get the mouse position
-        Vector2 mousePosition = GetGlobalMousePosition();
+		AddFish(new Vector2(random.Next(LeftMargin, RightMargin), random.Next(TopMargin, BottomMargin)));
+    }
+
+    private void AddFish(Vector2 position)
+    {        
         // create a new fish
         Fish newFish = fishScene.Instantiate() as Fish;
         // set the fish's position to the mouse position
-        newFish.Position = mousePosition;
+        newFish.Position = position;
         // add the fish to the scene
         AddChild(newFish);
 		// add the fish to the fishes list
